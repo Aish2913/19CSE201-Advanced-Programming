3fc94cdc65e30011a3f6f99e74fafb473dca34d7
Aishwarya.S <116823663+Aish2913@users.noreply.github.com>
2023-10-30 16:19:56 +0530

    Add files via upload

---
+++ 30.10.23_listfunctions.py
@@ -0,0 +1,17 @@
+neighbours=["adharsh","aditya","aish","amal"]
+neighbours.append("anu")
+print(neighbours)
+neighbours.remove("adharsh")
+neighbours.insert(1,"agil")
+print(neighbours)
+neighbours.sort()
+print(neighbours)
+neighbours.sort(reverse=True)
+print(neighbours)
+neighbours.clear()
+print(neighbours)
+del neighbours
+print(neighbours)
+
+
+ 
